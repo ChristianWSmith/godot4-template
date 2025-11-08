2ef00adf5d038267eea1fc5a469f3907ee81fb0d
Christian Smith <smith.christian.william@gmail.com>
2025-11-08 07:48:54 -0500

    input captor button bones

---
+++ scripts/ui/input_captor_button.gd
@@ -0,0 +1,9 @@
+extends Button
+
+@export var keyboard_allowed: bool = true
+@export var mouse_allowed: bool = true
+@export var joypad_allowed: bool = true
+@export var axes_allowed: bool = true
+
+func _ready() -> void:
+	pass
