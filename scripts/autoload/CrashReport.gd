60c3effc3af2a9fb6fad85b06e32ed5319cd63c8
Christian Smith <smith.christian.william@gmail.com>
2025-11-09 16:16:54 -0500

    free current scene on crash

--- scripts/autoload/CrashReport.gd
+++ scripts/autoload/CrashReport.gd
@@ -4,6 +4,7 @@ var crash_reason: String = ""
 
 func crash(source: String, reason: String) -> void:
 	crash_reason = "[%s] %s" % [source, reason]
+	get_tree().current_scene.queue_free()
 	get_tree().change_scene_to_file("res://scenes/crash.tscn")
 
 
