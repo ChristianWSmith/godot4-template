07739c3d88d469a7f5b2ad61c68c381ff2387ffa
Christian Smith <smith.christian.william@gmail.com>
2025-11-10 09:20:31 -0500

    better ui sounds

--- scripts/utils/ui_utils.gd
+++ scripts/utils/ui_utils.gd
@@ -13,55 +13,61 @@ static func tether_values(node_a: Control, node_b: Control):
 		Log.warn(node_a, "Failed to tether to '%s'" % node_b.name)
 
 
-static func connect_ui_sounds(node: Node):
-	var bound_click: Callable = AudioManager.play_ui.bind(preload("res://assets/bin/ui/click.wav"))
-	var bound_hover: Callable = AudioManager.play_ui.bind(preload("res://assets/bin/ui/hover.wav"))
-	var wrapped_click: Callable = func(_idx: int): AudioManager.play_ui(preload("res://assets/bin/ui/click.wav"))
-	var wrapped_hover: Callable = func(_idx: int): AudioManager.play_ui(preload("res://assets/bin/ui/hover.wav"))
-	for child in node.get_children():
-		connect_ui_sounds(child)
-		if child is SpinBox:
-			child.value_changed.connect(wrapped_click)
-			child.mouse_entered.connect(bound_hover)
-			child.set_meta("ui_sound_connections", {
-				"value_changed": wrapped_click,
-				"mouse_entered": bound_hover
-			})
-		elif child is Slider:
-			child.drag_started.connect(bound_click)
-			child.drag_ended.connect(wrapped_click)
-			child.mouse_entered.connect(bound_hover)
-			child.set_meta("ui_sound_connections", {
-				"drag_started": bound_click,
-				"drag_ended": wrapped_click,
-				"mouse_entered": bound_hover
-			})
-		elif child is TabContainer:
-			child.tab_selected.connect(wrapped_click)
-			child.tab_hovered.connect(wrapped_hover)
-			child.set_meta("ui_sound_connections", {
-				"tab_selected": wrapped_click,
-				"tab_hovered": wrapped_hover
-			})
-		elif child is OptionButton:
-			child.toggled.connect(wrapped_click)
-			child.mouse_entered.connect(bound_hover)
-			child.set_meta("ui_sound_connections", {
-				"toggled": wrapped_click,
-				"mouse_entered": bound_hover
-			})
-		elif child is Button:
-			child.pressed.connect(bound_click)
-			child.mouse_entered.connect(bound_hover)
-			child.set_meta("ui_sound_connections", {
-				"pressed": bound_click,
-				"mouse_entered": bound_hover
-			})
+static func connect_ui_sounds(
+		node: Node, 
+		deep: bool = true,
+		hover_stream: AudioStream = preload("res://assets/bin/ui/hover.wav"),
+		click_stream: AudioStream = preload("res://assets/bin/ui/click.wav")):
+	var bound_click: Callable = AudioManager.play_ui.bind(click_stream)
+	var bound_hover: Callable = AudioManager.play_ui.bind(hover_stream)
+	var wrapped_click: Callable = func(_idx: int): AudioManager.play_ui(click_stream)
+	var wrapped_hover: Callable = func(_idx: int): AudioManager.play_ui(hover_stream)
+	if deep:
+		for child in node.get_children():
+			connect_ui_sounds(child, deep, hover_stream, click_stream)
+	if node is SpinBox:
+		node.value_changed.connect(wrapped_click)
+		node.mouse_entered.connect(bound_hover)
+		node.set_meta("ui_sound_connections", {
+			"value_changed": wrapped_click,
+			"mouse_entered": bound_hover
+		})
+	elif node is Slider:
+		node.drag_started.connect(bound_click)
+		node.drag_ended.connect(wrapped_click)
+		node.mouse_entered.connect(bound_hover)
+		node.set_meta("ui_sound_connections", {
+			"drag_started": bound_click,
+			"drag_ended": wrapped_click,
+			"mouse_entered": bound_hover
+		})
+	elif node is TabContainer:
+		node.tab_selected.connect(wrapped_click)
+		node.tab_hovered.connect(wrapped_hover)
+		node.set_meta("ui_sound_connections", {
+			"tab_selected": wrapped_click,
+			"tab_hovered": wrapped_hover
+		})
+	elif node is OptionButton:
+		node.toggled.connect(wrapped_click)
+		node.mouse_entered.connect(bound_hover)
+		node.set_meta("ui_sound_connections", {
+			"toggled": wrapped_click,
+			"mouse_entered": bound_hover
+		})
+	elif node is Button:
+		node.pressed.connect(bound_click)
+		node.mouse_entered.connect(bound_hover)
+		node.set_meta("ui_sound_connections", {
+			"pressed": bound_click,
+			"mouse_entered": bound_hover
+		})
 
 
-static func clear_ui_sounds(node: Node):
-	for child in node.get_children():
-		clear_ui_sounds(child)
+static func clear_ui_sounds(node: Node, deep: bool = true):
+	if deep:
+		for child in node.get_children():
+			clear_ui_sounds(child, true)
 	if node.has_meta("ui_sound_connections"):
 		var data: Dictionary = node.get_meta("ui_sound_connections")
 		for sig in data.keys():
