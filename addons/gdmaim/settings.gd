ea63fa78416833b5304265aa6bda8d06c7043144
Christian Smith <smith.christian.william@gmail.com>
2025-11-19 08:25:32 -0500

    font fix

--- addons/gdmaim/settings.gd
+++ addons/gdmaim/settings.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6bd608638a1685c4a9029765a73f3e32729b777b2e8c9d0d504db60a84feab2
-size 7757
+oid sha256:ea9f41ce2221e00dce31c01d68fca09ac30d565ca9e08ee2115c28a145e9233e
+size 7756
